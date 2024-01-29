d3e5d2e5e861fa11ff21f89f34a6c1a746e6b545
jeenakarmi <jinanakarmi@gmail.com>
2024-01-29 19:19:22 +0545

    Implement Submit button functionality

--- login.py
+++ login.py
@@ -1,8 +1,27 @@
 from tkinter import *
+
+def get_val():
+    print(user_value.get())
+    print(password_value.get())
+
 root = Tk()
 root.geometry("655x333")
+root.title("Login Page")
 
-user = Label(root,text = "Username")
+user = Label(root,text = "Username",padx= 15)
 password = Label(root,text = "Password")
+user.grid()
+password.grid()
 
+user_value = StringVar()
+password_value = StringVar()
+
+user_entry = Entry(root,textvariable = user_value)
+password_entry = Entry(root,textvariable = password_value)
+
+#packing
+user_entry.grid(row = 0, column = 1)
+password_entry.grid(row = 1, column = 1)
+
+Button(text = "Submit",command = get_val).grid()
 root.mainloop()
