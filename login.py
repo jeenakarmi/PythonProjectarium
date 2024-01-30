595bc49f02971f08ab68d1a9a410242c305fa6cf
jeenakarmi <jinanakarmi@gmail.com>
2024-01-30 19:09:27 +0545

    bit nice lol

--- login.py
+++ login.py
@@ -6,27 +6,37 @@ def get_val():
 
 root = Tk()
 root.geometry("500x335")
+root.minsize(500, 275)
+root.maxsize(600, 200)
 root.title("Login Page")
-Label(root, text = "Welcome To Login Page", font = "comicsansms 13 bold" ).grid(row = 0, column = 3)
 
+# Centering everything
+root.columnconfigure(0, weight=1)
+root.columnconfigure(3, weight=2)
+root.rowconfigure(0, weight=0)
+root.rowconfigure(7, weight=0)
 
-user = Label(root,text = "Username")
-password = Label(root,text = "Password")
-user.grid()
-password.grid()
+Label(root, text="Welcome To Login Page", font="comicsansms 13 bold", pady=20).grid(row=0, column=2, sticky="nsew")
+
+user = Label(root, text="Username")
+password = Label(root, text="Password")
+user.grid(row=1, column=1, sticky="nsew")
+password.grid(row=2, column=1, sticky="nsew")
 
 user_value = StringVar()
-password_value = StringVar() 
+password_value = StringVar()
 remember_me = IntVar()
 
-user_entry = Entry(root,textvariable = user_value)
-password_entry = Entry(root,textvariable = password_value)
+user_entry = Entry(root, textvariable=user_value)
+password_entry = Entry(root, textvariable=password_value)
 
-#packing
-user_entry.grid(row = 1, column = 1)
-password_entry.grid(row = 2, column = 1)
+# packing
+user_entry.grid(row=1, column=2, sticky="nsew")
+password_entry.grid(row=2, column=2, sticky="nsew")
 
-Button(text = "Submit",command = get_val).grid()
-remember_me = Checkbutton(text = "Remember Me", variable= remember_me)
-remember_me.grid(row=4,column = 2)
+Button(text="Submit", command=get_val, bg="blue", fg="white").grid(row=6, column=2, sticky="nsew")
+
+remember_me = Checkbutton(text="Remember Login", variable=remember_me)
+remember_me.grid(row=4, column=2, sticky="nsew")
+
 root.mainloop()
