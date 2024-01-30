8323bd02a503378e141d9de4f914627c1cdcb985
jeenakarmi <jinanakarmi@gmail.com>
2024-01-30 18:16:02 +0545

    add checkbox

--- login.py
+++ login.py
@@ -5,23 +5,28 @@ def get_val():
     print(password_value.get())
 
 root = Tk()
-root.geometry("655x333")
+root.geometry("500x335")
 root.title("Login Page")
+Label(root, text = "Welcome To Login Page", font = "comicsansms 13 bold" ).grid(row = 0, column = 3)
 
-user = Label(root,text = "Username",padx= 15)
+
+user = Label(root,text = "Username")
 password = Label(root,text = "Password")
 user.grid()
 password.grid()
 
 user_value = StringVar()
-password_value = StringVar()
+password_value = StringVar() 
+remember_me = IntVar()
 
 user_entry = Entry(root,textvariable = user_value)
 password_entry = Entry(root,textvariable = password_value)
 
 #packing
-user_entry.grid(row = 0, column = 1)
-password_entry.grid(row = 1, column = 1)
+user_entry.grid(row = 1, column = 1)
+password_entry.grid(row = 2, column = 1)
 
 Button(text = "Submit",command = get_val).grid()
+remember_me = Checkbutton(text = "Remember Me", variable= remember_me)
+remember_me.grid(row=4,column = 2)
 root.mainloop()
