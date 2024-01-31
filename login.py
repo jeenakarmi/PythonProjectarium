e8b971897123dd8f50b6e83b272b461d718aaa72
jeenakarmi <jinanakarmi@gmail.com>
2024-01-31 20:09:00 +0545

    rvert

--- login.py
+++ login.py
@@ -4,14 +4,6 @@ def get_val():
     print(user_value.get())
     print(password_value.get())
 
-def toggle_password_visibility():
-    if show_password.get():
-        password_entry.config(show="")
-        show_password_label.config(text="👀")
-    else:
-        password_entry.config(show="*")
-        show_password_label.config(text="😎")
-
 root = Tk()
 root.geometry("500x335")
 root.minsize(500, 275)
@@ -34,10 +26,9 @@ password.grid(row=2, column=1, sticky="nsew")
 user_value = StringVar()
 password_value = StringVar()
 remember_me = IntVar()
-show_password = IntVar()
 
 user_entry = Entry(root, textvariable=user_value)
-password_entry = Entry(root, textvariable=password_value, show="*")
+password_entry = Entry(root, textvariable=password_value)
 
 # packing
 user_entry.grid(row=1, column=2, sticky="nsew")
@@ -47,11 +38,5 @@ Button(text="Submit", command=get_val, bg="blue", fg="white").grid(row=6, column
 
 remember_me = Checkbutton(text="Remember Login", variable=remember_me)
 remember_me.grid(row=4, column=2, sticky="nsew")
-
-show_password_label = Label(root)
-show_password_label.grid(row=2, column=3, sticky="w", padx=(0, 10))  # Adjust padx to move the emoji closer to the password entry
-
-show_password_checkbox = Checkbutton(text="Show  Password", variable=show_password, command=toggle_password_visibility)
-show_password_checkbox.grid(row=5, column=2, sticky="nsew")
 
 root.mainloop()
