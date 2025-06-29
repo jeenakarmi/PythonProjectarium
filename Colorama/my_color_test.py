4ffc869242714052bef26348643fc9cb4207ebb8
jeenakarmi <jinanakarmi@gmail.com>
2025-06-29 06:03:14 +0545

    Printing Colored Text

---
+++ Colorama/my_color_test.py
@@ -0,0 +1,8 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.MAGENTA + Back.WHITE + "Hi, my name is Jeena Nakarmi " + Fore.WHITE + Back.MAGENTA + "I am passionate about Machine Learning.")
+print(Fore.CYAN + Back.LIGHTYELLOW_EX + "Hi, my name is Jeena Nakarmi")
+print(Fore.GREEN + Back.LIGHTRED_EX + "Hi, my name is Jeena Nakarmi")
+print(Fore.YELLOW + Back.BLUE + "Hi, my name is Jeena Nakarmi")
